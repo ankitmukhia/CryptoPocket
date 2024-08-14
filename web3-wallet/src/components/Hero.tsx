c1eaee41f0359617f11c087b29f999c62ecbcccf
ankitmukhia <ankitmukhia157@gmail.com>
2024-08-14 15:53:47 +0530

    Added mnemonic some style

--- web3-wallet/src/components/Hero.tsx
+++ web3-wallet/src/components/Hero.tsx
@@ -2,13 +2,20 @@ import Card from "./ui/Card";
 import { useStore } from "../store";
 
 const Hero = () => {
-  //@ts-ignore
   const mnemonic = useStore((state) => state.mnemonicKeys)
+  const words = mnemonic.split(" ");
 
   return <div className="grid grid-cols-3 gap-2">
     <Card />
-    <div className="flex flex-row justify-center items-center border border-[#A5A5A540] px-2 rounded-md col-span-2">
-      <p className="text-white text-sm">{mnemonic ? mnemonic : "Create Mnemonic"}</p>
+    <div className="grid grid-cols-3 hover:bg-gray-800/50 cursor-pointer gap-2 border border-[#A5A5A540] px-2 rounded-md col-span-2">
+      {words.map((word, index) => (
+        <div key={index} className="flex justify-start items-center p-1 space-x-2">
+          {word && (
+            <div className="text-gray-400 text-sm">{index + 1}.</div>
+          )}
+          <p className="text-white text-sm">{word ? word : "Create Mnemonic"}</p>
+        </div>
+      ))}
     </div>
   </div>
 }
